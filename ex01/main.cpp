a94a20ccb002c6c79b733c134dca3d9b6a38f7a6
Milan Forsthövel <mforstho@student.codam.nl>
2023-03-21 18:02:00 +0100

    Added second test to ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mforstho <mforstho@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/31 15:23:11 by mforstho      #+#    #+#                 */
-/*   Updated: 2023/01/31 16:09:49 by mforstho      ########   odam.nl         */
+/*   Updated: 2023/03/21 17:59:37 by mforstho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,25 @@ void leakfunc(void) {
 
 int	main(void) {
 	atexit(leakfunc);
-	int	n = 6;
+	{
+		int	n = 6;
 
-	Zombie *horde = zombieHorde(n, "chumper");
-	for (int i = 0; i < n; i++) {
-		std::cout << i << ": ";
-		horde[i].announce();
+		Zombie *horde = zombieHorde(n, "chumper");
+		for (int i = 0; i < n; i++) {
+			std::cout << i << ": ";
+			horde[i].announce();
+		}
+		delete[] horde;
 	}
-	delete[] horde;
+	{
+		int	n = 0;
+
+		Zombie *horde = zombieHorde(n, "bob");
+		for (int i = 0; i < n; i++) {
+			std::cout << i << ": ";
+			horde[i].announce();
+		}
+		delete[] horde;
+	}
 	return (EXIT_SUCCESS);
 }
